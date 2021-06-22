99261c52059e102747bf32ad36cfa50be9fd3ce4
Durrell Jules <dujules23@gmail.com>
2021-06-22 16:09:01 -0400

    created title component, re-organized index.js

---
+++ app/src/components/Title.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+
+function Title() {
+  return (
+    <div className="Title">
+      <h1>Breath Of The Wild Monsters</h1>
+    </div>
+  );
+}
+
+export default Title;
